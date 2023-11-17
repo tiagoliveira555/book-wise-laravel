8fe5046d2cefbc31c8f04748f70425f8f2e1105c
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-11-17 19:05:22 -0300

    alter favicon ico

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,7 +9,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,400;6..12,700&display=swap" rel="stylesheet">
-
+        <link rel="shortcut icon" href="favicon.svg" type="image/x-icon">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased max-w-[1440px] min-h-screen mx-auto p-5 bg-base-gray-800 text-white">
