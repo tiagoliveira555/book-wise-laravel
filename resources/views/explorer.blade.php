d91404abfc0a586fd1ca2d5f7fad2defcbe6c37d
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-11-20 08:59:15 -0300

    add search filter books

--- resources/views/explorer.blade.php
+++ resources/views/explorer.blade.php
@@ -60,11 +60,23 @@
                         <i class="text-3xl text-base-green-100 ph ph-binoculars"></i>
                         <h2 class="text-base-gray-100 text-2xl font-bold">Explorar</h2>
                     </div>
-                    <div class="w-[433px] h-12 rounded py-[14px] px-5 border border-base-gray-500 focus-within:border-base-green-200 flex justify-between items-center">
-                        <input class="bg-transparent text-base-gray-200 text-sm outline-none border-none focus:ring-0 placeholder:text-base-gray-400 peer" type="text" placeholder="Buscar livro ou autor">
+                    <div class="w-[433px] h-12 rounded py-[14px] px-5 border border-base-gray-500 focus-within:border-base-green-200 flex justify-between items-center transition-all">
+                        <input class="flex-1 bg-transparent text-base-gray-200 text-sm outline-none border-none focus:ring-0 placeholder:text-base-gray-400 peer" type="text" placeholder="Buscar livro ou autor">
                         <i class="text-xl text-base-gray-500 ph ph-magnifying-glass peer-focus:text-base-green-200"></i>
                     </div>
                 </header>
+                <div class="mt-10">
+                    <ul class="flex items-center gap-3">
+                        <li class="py-1 px-4 rounded-full bg-base-purple-200 text-center text-base-gray-100 border border-transparent hover:border-base-purple-100 transition-all">Tudo</li>
+                        <li class="py-1 px-4 rounded-full text-center text-base-purple-100 border border-base-purple-100 hover:border-base-purple-100 transition-all">Computação</li>
+                        <li class="py-1 px-4 rounded-full text-center text-base-purple-100 border border-base-purple-100 hover:border-base-purple-100 transition-all">Educação</li>
+                        <li class="py-1 px-4 rounded-full text-center text-base-purple-100 border border-base-purple-100 hover:border-base-purple-100 transition-all">Fantasia</li>
+                        <li class="py-1 px-4 rounded-full text-center text-base-purple-100 border border-base-purple-100 hover:border-base-purple-100 transition-all">Ficção científica</li>
+                        <li class="py-1 px-4 rounded-full text-center text-base-purple-100 border border-base-purple-100 hover:border-base-purple-100 transition-all">Horror</li>
+                        <li class="py-1 px-4 rounded-full text-center text-base-purple-100 border border-base-purple-100 hover:border-base-purple-100 transition-all">HQs</li>
+                        <li class="py-1 px-4 rounded-full text-center text-base-purple-100 border border-base-purple-100 hover:border-base-purple-100 transition-all">Suspense</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </body>
