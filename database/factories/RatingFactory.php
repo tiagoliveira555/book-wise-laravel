0db6cb361b4608d10f420fbacd3f326d04f86d3d
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-11-22 19:37:07 -0300

    finally seeders

--- database/factories/RatingFactory.php
+++ database/factories/RatingFactory.php
@@ -4,14 +4,14 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Rating>
- */
 class RatingFactory extends Factory
 {
     public function definition(): array
     {
         return [
+            'id' => fake()->uuid(),
+            'rate' => fake()->numberBetween(1, 5),
+            'description' => fake()->text(),
         ];
     }
 }
