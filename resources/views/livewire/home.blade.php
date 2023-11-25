f155e47748b0b5216e0e63363de5db1a6ce1496b
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-11-25 19:54:50 -0300

    component popular book

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -40,26 +40,7 @@
         </header>
 
         @foreach ($this->popularBooks as $popularBook)
-        <div class="mt-3 flex rounded-lg py-4 px-5 gap-5 bg-base-gray-700">
-            <img
-                class="w-16 h-[94px]"
-                src="{{ $popularBook->cover_url }}"
-                alt="{{ $popularBook->name }}"
-            >
-            <div class="flex flex-col justify-between items-start">
-                <div>
-                    <h2 class="font-bold text-base-gray-100">{{ $popularBook->name }}</h2>
-                    <h3 class="text-sm text-base-gray-400">{{ $popularBook->author }}</h3>
-                </div>
-                <ul class="flex justify-center items-center gap-1 text-base-purple-100">
-                    <li><i class="ph-fill ph-star"></i></li>
-                    <li><i class="ph-fill ph-star"></i></li>
-                    <li><i class="ph-fill ph-star"></i></li>
-                    <li><i class="ph-fill ph-star"></i></li>
-                    <li><i class="ph ph-star"></i></li>
-                </ul>
-            </div>
-        </div>
+            <x-popular-book :$popularBook />
         @endforeach
 
     </div>
