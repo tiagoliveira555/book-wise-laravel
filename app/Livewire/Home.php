9416448b3b1d1e4b513989460e3b63a2503582c6
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-11-24 21:25:15 -0300

    refactor Home

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -4,26 +4,30 @@ namespace App\Livewire;
 
 use App\Models\Rating;
 use Carbon\Carbon;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Home extends Component
 {
-    public array $ratings = [];
+    public mixed $ratings = [];
 
-    public function render()
+    public function render(): View
     {
-        $ratings = Rating::query()
-                        ->orderBy('created_at', 'desc')
-                        ->with(['user:id,name,avatar_url', 'book:id,name,author,summary,cover_url'])
-                        ->limit(10)
-                        ->get();
-
         return view('livewire.home', [
-            $this->ratings = $ratings->toArray(),
+            $this->ratings = $this->getLastTenRatings(),
         ]);
     }
 
-    public function convertDateForHumans($date)
+    private function getLastTenRatings()
+    {
+        return Rating::query()
+        ->orderBy('created_at', 'desc')
+        ->with(['user:id,name,avatar_url', 'book:id,name,author,summary,cover_url'])
+        ->limit(10)
+        ->get();
+    }
+
+    public function convertDateForHumans(string $date): string
     {
         Carbon::setLocale('pt_BR');
 
--- resources/views/components/latest-reviews.blade.php
+++ resources/views/components/latest-reviews.blade.php
@@ -2,11 +2,11 @@
     <div class="flex justify-between items-start">
         <div class="flex gap-4">
             <a href="{{ route('profile') }}" wire:navigate class="h-10 w-10 flex justify-center items-center bg-gradient-vertical rounded-full cursor-pointer">
-                <img class="rounded-full w-9 h-9" src="{{ $rating['user']['avatar_url'] }}" alt="{{ $rating['user']['name'] }}">
+                <img class="rounded-full w-9 h-9" src="{{ $rating->user->avatar_url }}" alt="{{ $rating->user->name }}">
             </a>
             <div>
-                <h3 class="text-base-gray-100">{{ $rating['user']['name'] }}</h3>
-                <h4 class="text-sm text-base-gray-400">{{ $this->convertDateForHumans($rating['created_at']) }}</h4>
+                <h3 class="text-base-gray-100">{{ $rating->user->name }}</h3>
+                <h4 class="text-sm text-base-gray-400">{{ $this->convertDateForHumans($rating->created_at) }}</h4>
             </div>
         </div>
         <ul class="flex justify-center items-center gap-1 text-base-purple-100">
@@ -20,13 +20,13 @@
     <div class="mt-8 flex gap-5">
         <img
             class="w-[108px] h-[152px]"
-            src="{{ $rating['book']['cover_url'] }}"
-            alt="{{ $rating['book']['name'] }}"
+            src="{{ $rating->book->cover_url }}"
+            alt="{{ $rating->book->name }}"
         >
         <div>
-            <h2 class="font-bold text-base-gray-100">{{ $rating['book']['name'] }}</h2>
-            <h3 class="text-sm text-base-gray-400">{{ $rating['book']['author'] }}</h3>
-            <p class="mt-5 text-base-gray-300 text-sm leading-[22px]">{{ $rating['book']['summary'] }}</p>
+            <h2 class="font-bold text-base-gray-100">{{ $rating->book->name }}</h2>
+            <h3 class="text-sm text-base-gray-400">{{ $rating->book->author }}</h3>
+            <p class="mt-5 text-base-gray-300 text-sm leading-[22px]">{{ $rating->book->summary }}</p>
         </div>
     </div>
 </div>
