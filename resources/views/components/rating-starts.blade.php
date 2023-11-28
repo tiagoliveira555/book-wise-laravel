631dd5932007f828c15cc9519d1a0ff3ee7e698f
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-11-28 16:37:56 -0300

    create component rating-starts

---
+++ resources/views/components/rating-starts.blade.php
@@ -0,0 +1,38 @@
+@props([
+    'rate' => 0,
+    'isEditable' => false
+])
+
+<ul
+    x-data="{
+        rating: {{ $rate }},
+        isEditable: {{ $isEditable ? 'true' : 'false' }},
+        previewValue: this.isEditable ? 0 : {{ $rate }},
+        handleMouseEnter(value) {
+            if(this.isEditable) this.previewValue = value
+        },
+        handleMouseLeave() {
+            if(this.isEditable) this.previewValue = this.rating
+        },
+        handleRate() {
+            this.rating = this.previewValue
+        }
+    }"
+
+    class="flex justify-center items-center gap-1 text-base-purple-100"
+    x-bind:class="{ 'cursor-pointer': isEditable }"
+>
+    @foreach (range(1, 5) as $index)
+    <li>
+        <i
+            class="ph-star"
+            x-bind:class="{ 'ph-fill': {{ $index }} <= previewValue, 'ph': {{ $index }} > previewValue }"
+            x-on:mouseenter="handleMouseEnter({{ $index }})"
+            x-on:mouseleave="handleMouseLeave"
+            x-on:click="handleRate"
+        >
+        </i>
+    </li>
+    @endforeach
+</ul>
+
