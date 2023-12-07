8d2db8c27e58930cf16701b80eed159c163304e9
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-12-07 18:27:11 -0300

    refactor in card profile add rate stars

--- resources/views/components/card-profile-rating.blade.php
+++ resources/views/components/card-profile-rating.blade.php
@@ -8,7 +8,7 @@
                     <h2 class="text-lg font-bold text-base-gray-100">{{ $rating->book->name }}</h2>
                     <h3 class="text-sm text-base-gray-400">{{ $rating->book->author }}</h3>
                 </div>
-                <x-rating-stars rate="4" />
+                <x-rating-stars rate="{{ $rating->rate }}" />
             </div>
         </div>
         <p class="text-base-gray-300 text-sm">{{ $rating->description }}</p>
