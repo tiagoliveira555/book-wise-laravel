b58454c7eaa78be5f93f5ec7d16089eacbc56903
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-12-08 13:23:20 -0300

    refactor in rating description

--- resources/views/components/rating-description.blade.php
+++ resources/views/components/rating-description.blade.php
@@ -11,7 +11,7 @@
 <div wire:key="{{ $rating->id }}" class="{{ $bg }} flex flex-col gap-5 p-6 rounded-lg">
     <div class="flex justify-between items-start gap-4">
         <div class="flex gap-4">
-            <a href="{{ route('profile') }}" wire:navigate class="h-10 w-10 flex justify-center items-center bg-gradient-vertical rounded-full cursor-pointer">
+            <a href="{{ route('profile', ['id' => $rating->user->id]) }}" wire:navigate class="h-10 w-10 flex justify-center items-center bg-gradient-vertical rounded-full cursor-pointer">
                 <img class="rounded-full w-9 h-9" src="{{ $rating->user->avatar_url }}" alt="{{ $rating->user->name }}">
             </a>
             <div>
